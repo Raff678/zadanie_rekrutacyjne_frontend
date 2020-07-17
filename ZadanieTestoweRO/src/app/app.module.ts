96bbbd0d48e7cf52341655f4806f585f0eeb6a2e
Rafał Owczarski <rowczarski11@gmail.com>
2020-07-17 19:51:15 +0200

    main grid created

--- ZadanieTestoweRO/src/app/app.module.ts
+++ ZadanieTestoweRO/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainSectionComponent } from './main-section/main-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainSectionComponent
   ],
   imports: [
     BrowserModule,
